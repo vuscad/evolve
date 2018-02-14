36142df5d2a3c5631b304ef062d5883818a3deda
danvusca <dan.vusca@cegeka.com>
2018-02-14 14:16:03 +0200

    vuscad: remove sub-areas class, change model to kotlin

---
+++ src/main/java/com/objective/evolve/model/Domain.kt
@@ -0,0 +1,13 @@
+package com.objective.evolve.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.TypeAlias
+import org.springframework.data.mongodb.core.mapping.DBRef
+import org.springframework.data.mongodb.core.mapping.Document
+import javax.validation.constraints.NotNull
+
+@Document(collection = "area")
+@TypeAlias("area")
+data class Domain constructor(@Id var id: String?, @NotNull var name: String?, @DBRef var tasks: List<Task>?) {
+    private constructor() : this(null, null, null)
+}
---
+++ src/main/java/com/objective/evolve/model/Task.kt
@@ -0,0 +1,12 @@
+package com.objective.evolve.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.TypeAlias
+import org.springframework.data.mongodb.core.mapping.Document
+import javax.validation.constraints.NotNull
+
+@Document(collection = "task")
+@TypeAlias("task")
+data class Task constructor(@Id var id: String?, @NotNull var name: String?, var assignments: List<String>?) {
+    private constructor() : this(null, null, null)
+}
